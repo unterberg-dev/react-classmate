0e0cb702572c0868ea6732597d32872a4e73cc65
Richard Unterberg <mail@richardunterberg.de>
2024-10-18 08:42:27 +0200

    🔥 change to new design

--- README.md
+++ README.md
@@ -1,41 +1,39 @@
-# react-styled-classnames
+# react-dynamic-style
 
-Separate styles/classes from your react-components - the simple way 🫰
+Separate styles and classes from your React components, seamlessly integrating with utility-first CSS libraries like UnoCSS and Tailwind. 🫰
 
 ## The problem
 
-While developing with react and [uno.css](https://unocss.dev/) / [tailwind](https://tailwindcss.com/), I found myself writing utility classes and conditions directly in the components `className` attribute:
+When working with styling libraries like [uno.css](https://unocss.dev/) / [tailwind](https://tailwindcss.com/), it's common to define utility classes directly in your React components. While this approach works for most of our cases, it can lead to cluttered and hard-to-maintain code, especially when dealing with conditional classes, dynamic styles, or complex animations.
 
 ```tsx
 <div className={`text-white text-2xl font-bold ${isActive ? 'animate-in fade-in' : 'animate-out fade-out'}`}>Hello</div>
 ```
 
-That made the components hard to read and maintain, since we often need conditional classes and animations.
+## What the tool does
 
-## The mission
-
-- simple utility to separate class names from the components
-- SSR-compatible
-- work with any class name, including tailwind or unocss (presets).
-- no `styled-components` or `emotion` libs
-- pass (dynamic) css objects 
+- Compatible with SSR (Server-Side Rendering)
+- Works seamlessly with popular utility-first libraries like UnoCSS and Tailwind
+- Allows passing dynamic CSS objects, in addition to class names
+- No dependency on styled-components or emotion
+- TS Support (WIP)
 
 ### Not re-inventing the wheel
 
-There are libs outside that do a great job in this area, like [twin.macro](https://github.com/ben-rogerson/twin.macro) or [tailwind-styled-components](https://github.com/MathiasGilson/tailwind-styled-component), but they are either too complex, using `styled-components` or are not SSR-compatible. Additionally I like to work with a little bit more separation of concerns when it comes to conditional classes, as you can see in the example below.
+There are other libraries that handle this area well, such as [twin.macro](https://github.com/ben-rogerson/twin.macro)  and [tailwind-styled-components](https://github.com/MathiasGilson/tailwind-styled-component). However, these solutions are either too complex, rely on `styled-components`, or lack SSR compatibility. I prefer a simpler approach with more separation of concerns for handling conditional classes, as demonstrated in the example below.
 
 ## Getting started
 
 ```bash
-npm i react-styled-classnames # or yarn
+npm i react-dynamic-style # or yarn
 ```
 
 ### Simple usage
 
 ```tsx
-import sc from 'react-styled-classnames'
+import { ds } from 'react-dynamic-style'
 
-const StyledDiv = sc('div', 'text-xl bg-blue-500');
+const StyledDiv = ds('div', 'text-xl bg-blue-500');
 
 const SomeComponent = () => 
   <div>
@@ -46,14 +44,14 @@ const SomeComponent = () =>
 ### Advanced usage
 
 ```tsx
-import sc from 'react-styled-classnames'
+import { ds } from 'react-dynamic-style'
 
 // props interface
 interface StyledDivProps {
   $isActive?: boolean
 }
 
-const StyledDiv = sc<StyledDivProps>(
+const StyledDiv = ds<StyledDivProps>(
   // component tag
   'div',
   {
@@ -83,16 +81,16 @@ const SomeComponent = () => {
 Atm you can pass props wildly (typescript does not check the prop types), which produces evtl. invalid html like this:
 
 ```tsx
-const MyElement = sc('div', 'text-xl bg-blue-500' );
+const MyElement = ds('div', 'text-xl bg-blue-500' );
 
-const SomeComponent = () => <MyElement src="hello-world">Weird prop pass</MyElement>
+const SomeComponent = () => <MyElement src="hello-world">Invalid src prop pass</MyElement>
 
 // this creates html with a invalid property "src":
 // <div src="hello-world" class="text-xl bg-blue-500">...
 ```
 
 ## Upcoming features
-- typescript should emit error if we use invalid properties (see above)
+- typescript should emit error if we use invalid properties (see above) ⬆️
 
 ## Inspired by:
 - [twin.macro](https://github.com/ben-rogerson/twin.macro)
--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,74 @@
-import clsx from 'clsx';
-import React, { forwardRef, CSSProperties } from 'react';
+import clsx from 'clsx'
+import React, { CSSProperties, forwardRef } from 'react'
 
-const cleanCssString = (css: string): string => {
-  return css
-    .replace(/\s+/g, ' ')
-    .trim();
-};
+const cleanCssString = (css: string): string => css.replace(/\s+/g, ' ').trim()
 
 type WithHTMLProps<T> = T & React.HTMLProps<HTMLElement>
 
-interface SCOptions {
-  base?: string;
-  classes?: (props: WithHTMLProps<any>) => string[];
-  css?: CSSProperties | ((props: WithHTMLProps<any>) => CSSProperties); // Updated to allow function
+// Define a type alias for the repeated type
+type ForwardRefComponent<T> = React.ForwardRefExoticComponent<WithHTMLProps<T> & React.RefAttributes<HTMLElement>>
+
+interface DsComponentOptions {
+  base?: string
+  classes?: (props: WithHTMLProps<any>) => string[]
+  css?: CSSProperties | ((props: WithHTMLProps<any>) => CSSProperties) // Updated to allow function
 }
 
-type SCType = <T>(
+type DsComponentProps = <T>(
   tag: keyof JSX.IntrinsicElements,
-  opts: SCOptions | string, // Updated to allow string
-) => React.ForwardRefExoticComponent<WithHTMLProps<T> & React.RefAttributes<HTMLElement>>;
+  opts: DsComponentOptions | string, // Updated to allow string
+) => ForwardRefComponent<T>
 
 /**
- * A higher-order function that creates a React component with dynamic class names.
+ * A higher-order function that creates a React component with dynamic class names or style.
  *
  * @param {keyof JSX.IntrinsicElements} tag - The HTML tag to be used for the component (e.g., 'div', 'span').
  * @param {SCOptions | string} opts - The options for the component or a string representing the base class.
- * @returns {React.ForwardRefExoticComponent<WithHTMLProps<T> & React.RefAttributes<HTMLElement>>} - A styled React component with dynamic class names.
+ * @returns {ForwardRefComponent<T>} - A styled React component with dynamic class names.
  */
-const sc: SCType = <T>(
+export const ds: DsComponentProps = <T>(
   tag: keyof JSX.IntrinsicElements,
-  opts: SCOptions | string,
-): React.ForwardRefExoticComponent<
-  WithHTMLProps<T> & React.RefAttributes<HTMLElement>
-> => {
+  opts: DsComponentOptions | string,
+): ForwardRefComponent<T> => {
   // Determine if opts is a string or an object
-  const baseClass = typeof opts === 'string' ? opts : opts.base;
-  const dynamicClasses = typeof opts === 'object' ? opts.classes : undefined;
-  const css = typeof opts === 'object' ? opts.css : undefined;
+  const baseClass = typeof opts === 'string' ? opts : opts.base
+  const dynamicClasses = typeof opts === 'object' ? opts.classes : undefined
+  const css = typeof opts === 'object' ? opts.css : undefined
 
-  const RenderComponent = forwardRef<HTMLElement, WithHTMLProps<T>>(
-    (props, ref) => {
-      const { className, style, ...restProps } = props;
+  const RenderComponent = forwardRef<HTMLElement, WithHTMLProps<T>>((props, ref) => {
+    const { className, style, ...restProps } = props
 
-      // Handle dynamic class names
-      const classes = cleanCssString(clsx(
+    // Handle dynamic class names
+    const classes = cleanCssString(
+      clsx(
         baseClass,
         dynamicClasses ? dynamicClasses(restProps as WithHTMLProps<T>) : [],
         className,
-      ));
+      ),
+    )
 
-      // Filter out invalid HTML attributes and properties prefixed with "$"
-      const filteredProps = Object.fromEntries(
-        Object.entries(restProps).filter(([key]) => 
-          !key.startsWith('$')
-        ),
-      );
+    // Filter out invalid HTML attributes and properties prefixed with "$"
+    const filteredProps = Object.fromEntries(
+      Object.entries(restProps).filter(([key]) => !key.startsWith('$')),
+    )
 
-      // Handle dynamic CSS
-      const resolvedCss = typeof css === 'function' ? css(restProps as WithHTMLProps<T>) : css;
+    // Handle dynamic CSS
+    const resolvedCss = typeof css === 'function' ? css(restProps as WithHTMLProps<T>) : css
 
-      // Merge styles
-      const mergedStyles = { ...resolvedCss, ...style };
+    // Merge styles
+    const mergedStyles = { ...resolvedCss, ...style }
 
-      // generate component
-      return React.createElement(
-        tag,
-        {
-          className: classes,
-          style: mergedStyles,
-          ref,
-          ...filteredProps,
-        },
-      );
-    },
-  );
+    // generate component
+    return React.createElement(tag, {
+      className: classes,
+      style: mergedStyles,
+      ref,
+      ...filteredProps,
+    })
+  })
 
   // debug
-  RenderComponent.displayName = `StyledComponent(${tag})`;
+  RenderComponent.displayName = `reactDynamicStyleComponent(${tag})`
 
-  return RenderComponent as React.ForwardRefExoticComponent<
-    WithHTMLProps<T> & React.RefAttributes<HTMLElement>
-    >;
-};
-
-export default sc;
+  return RenderComponent as ForwardRefComponent<T>
+}
