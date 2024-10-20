5bd35526c56fb5de5a830c49ff7835a9ece06c1e
Richard Unterberg <mail@richardunterberg.de>
2024-10-20 22:34:12 +0200

    🐛 properly type the incoming style props, fix autocomplete

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,108 @@
 import clsx from 'clsx'
-import React, { CSSProperties, forwardRef } from 'react'
+import React, {
+  CSSProperties,
+  forwardRef,
+  ForwardRefExoticComponent,
+  JSX,
+  RefAttributes,
+} from 'react'
 
-type WithHTMLProps<T, K extends keyof JSX.IntrinsicElements> = T & JSX.IntrinsicElements[K]
-
-interface DsComponentOptions {
+type RefComponent<T> = ForwardRefExoticComponent<T & RefAttributes<HTMLElement>>
+type ComponentOptions<T> = {
   base?: string
-  classes?: (props: any) => string[]
-  css?: CSSProperties | ((props: any) => CSSProperties)
+  classes?: (props: T) => string[]
+  css?: CSSProperties | ((props: T) => CSSProperties)
 }
 
-// Helper type to conditionally apply intrinsic element props (HTML vs SVG)
-type FilterValidProps<T, K extends keyof JSX.IntrinsicElements> = K extends keyof JSX.IntrinsicElements
+type HTMLProps<T, K extends keyof JSX.IntrinsicElements> = T & JSX.IntrinsicElements[K]
+type ValidProps<T, K extends keyof JSX.IntrinsicElements> = K extends keyof JSX.IntrinsicElements
   ? T & JSX.IntrinsicElements[K]
   : never
 
-type ForwardRefComponent<T> = React.ForwardRefExoticComponent<T & React.RefAttributes<HTMLElement>>
+const cleanClassName = (className: string) => className.replace(/\s+/g, ' ').trim()
 
-const createComponent = <T, K extends keyof JSX.IntrinsicElements = keyof JSX.IntrinsicElements>(
+const omitCustomProps = <T extends object, K extends keyof JSX.IntrinsicElements>(
+  props: T,
+  customProps: (keyof T)[],
+): JSX.IntrinsicElements[K] => {
+  const domProps: Record<string, any> = {}
+  for (const key in props) {
+    if (!customProps.includes(key as keyof T)) {
+      domProps[key] = props[key]
+    }
+  }
+  return domProps as JSX.IntrinsicElements[K]
+}
+
+const createComponent = <
+  T extends object,
+  K extends keyof JSX.IntrinsicElements = keyof JSX.IntrinsicElements,
+>(
   tag: K,
-  opts?: DsComponentOptions | string | ((props: WithHTMLProps<T, K>) => string),
-  css?: ((props: WithHTMLProps<T, K>) => CSSProperties) | CSSProperties,
-): ForwardRefComponent<FilterValidProps<T, K>> => {
-  const baseClass = typeof opts === 'string' ? opts : typeof opts === 'object' ? opts?.base : undefined
-  const dynamicClasses = typeof opts === 'object' ? opts?.classes : typeof opts === 'function' ? opts : undefined
-  const dynamicCss = typeof opts === 'object' ? opts?.css : css
+  options?: ComponentOptions<T> | string | ((props: HTMLProps<T, K>) => string),
+  css?: ((props: HTMLProps<T, K>) => CSSProperties) | CSSProperties,
+): RefComponent<ValidProps<T, K>> => {
+  let baseClass: string | undefined
+  if (typeof options === 'string') {
+    baseClass = options
+  } else if (typeof options === 'object') {
+    baseClass = options?.base
+  } else {
+    baseClass = undefined
+  }
 
-  const RenderComponent = forwardRef<HTMLElement, FilterValidProps<T, K>>((props, ref) => {
+  let dynamicClasses: string | Function | undefined
+  if (typeof options === 'object') {
+    dynamicClasses = options?.classes
+  } else if (typeof options === 'function') {
+    dynamicClasses = options
+  } else {
+    dynamicClasses = undefined
+  }
+
+  const dynamicCss = typeof options === 'object' ? options?.css : css
+
+  const RenderComponent = forwardRef<HTMLElement, ValidProps<T, K>>((props, ref) => {
     const { className, style, ...restProps } = props
 
-    // Generate the className string dynamically using clsx
-    const classes = clsx(
-      baseClass,
-      dynamicClasses ? dynamicClasses(props as WithHTMLProps<T, K>) : [],
-      className
+    const classes = cleanClassName(
+      clsx(baseClass, dynamicClasses ? dynamicClasses(props as HTMLProps<T, K>) : [], className),
     )
 
-    // Resolve CSS based on props or static values
-    const resolvedCss = typeof dynamicCss === 'function' ? dynamicCss(props as WithHTMLProps<T, K>) : dynamicCss
+    const resolvedCss =
+      typeof dynamicCss === 'function' ? dynamicCss(props as HTMLProps<T, K>) : dynamicCss
     const mergedStyles = { ...resolvedCss, ...style }
 
+    const customProps = Object.keys(props).filter(key => key.startsWith('$')) as (keyof T)[]
+    const domProps = omitCustomProps<T, K>(restProps as T, customProps)
+
     return React.createElement(tag, {
-      ...restProps,
+      ...domProps,
       className: classes,
       style: mergedStyles,
-      ref
+      ref,
     })
   })
 
   RenderComponent.displayName = `DynamicComponent(${tag})`
-  return RenderComponent as ForwardRefComponent<FilterValidProps<T, K>>
+
+  return RenderComponent as RefComponent<ValidProps<T, K>>
 }
 
-export const ds = new Proxy({}, {
-  get: (_, tag: keyof JSX.IntrinsicElements) => {
-    return <T, K extends keyof JSX.IntrinsicElements = typeof tag>(
-      opts?: DsComponentOptions | string | ((props: WithHTMLProps<T, K>) => string),
-      css?: ((props: WithHTMLProps<T, K>) => CSSProperties) | CSSProperties
-    ) => createComponent<T, K>(tag as K, opts, css)
-  }
-}) as {
+export const ds = new Proxy(
+  {},
+  {
+    get:
+      (_, tag: keyof JSX.IntrinsicElements) =>
+      <T extends object, K extends keyof JSX.IntrinsicElements = typeof tag>(
+        options?: ComponentOptions<T> | string | ((props: HTMLProps<T, K>) => string),
+        css?: ((props: HTMLProps<T, K>) => CSSProperties) | CSSProperties,
+      ) =>
+        createComponent<T, K>(tag as K, options, css),
+  },
+) as {
   [K in keyof JSX.IntrinsicElements]: <T, E extends keyof JSX.IntrinsicElements = K>(
-    opts?: DsComponentOptions | string | ((props: WithHTMLProps<T, E>) => string),
-    css?: ((props: WithHTMLProps<T, E>) => CSSProperties) | CSSProperties
-  ) => ForwardRefComponent<FilterValidProps<T, E>>
+    options?: ComponentOptions<T> | string | ((props: HTMLProps<T, E>) => string),
+    css?: ((props: HTMLProps<T, E>) => CSSProperties) | CSSProperties,
+  ) => RefComponent<ValidProps<T, E>>
 }
