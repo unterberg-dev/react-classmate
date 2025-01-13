30967261a161ddeaf1514910207cf64affaead73
Richard Unterberg <mail@richardunterberg.de>
2025-01-13 15:36:07 +0100

    🔥 rm babel rollup, mark no side effects

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import babel from "@rollup/plugin-babel"
 import commonjs from "@rollup/plugin-commonjs"
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript"
@@ -26,10 +25,10 @@ export default [
       typescript({
         tsconfig: "./tsconfig.json",
       }),
-      babel({
-        babelHelpers: "bundled",
-        extensions: [".ts", ".tsx"],
-      }),
+      // babel({
+      //   babelHelpers: "bundled",
+      //   extensions: [".ts", ".tsx"],
+      // }),
       minify(),
     ],
     external: ["react", "react-dom"],
