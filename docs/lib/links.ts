a8642873ba4ccf295dfa4bd6f0202d63fa8e81a3
Richard Unterberg <mail@richardunterberg.de>
2025-01-14 20:18:48 +0100

    🐛 trailing slash links

--- docs/lib/links.ts
+++ docs/lib/links.ts
@@ -5,22 +5,22 @@ const base = APP_CONFIG.viteUrl
 export const internalLink = {
   start: `${APP_CONFIG.viteUrl}`,
   docs: {
-    getStarted: `${base}/docs/get-started`,
-    basic: `${base}/docs/basic`,
-    extend: `${base}/docs/extend`,
-    variants: `${base}/docs/variants`,
+    getStarted: `${base}/docs/get-started/`,
+    basic: `${base}/docs/basic/`,
+    extend: `${base}/docs/extend/`,
+    variants: `${base}/docs/variants/`,
   },
   util: {
-    style: `${base}/docs/interpolation/style`,
-    convertRcProps: `${base}/docs/helper/convertrcprops`,
-    createVariantMap: `${base}/docs/helper/createvariantmap`,
+    style: `${base}/docs/interpolation/style/`,
+    convertRcProps: `${base}/docs/helper/convertrcprops/`,
+    createVariantMap: `${base}/docs/helper/createvariantmap/`,
   },
   typescript: {
-    rcBaseComponent: `${base}/docs/typescript/rcbasecomponent`,
-    variantsConfig: `${base}/docs/typescript/variantsconfig`,
+    rcBaseComponent: `${base}/docs/typescript/rcbasecomponent/`,
+    variantsConfig: `${base}/docs/typescript/variantsconfig/`,
   },
   example: {
-    advancedButton: `${base}/docs/examples/advanced-button`,
-    link: `${base}/docs/examples/advanced-button`,
+    advancedButton: `${base}/docs/examples/advanced-button/`,
+    link: `${base}/docs/examples/advanced-button/`,
   },
 }
