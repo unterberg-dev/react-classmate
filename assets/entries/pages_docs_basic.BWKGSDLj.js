088b18c9132e2b4e33b8452bf5ff31ecf8ca291b
richard-unterberg <richard-unterberg@users.noreply.github.com>
2025-01-21 19:44:04 +0000

    deploy: 80254ad1e30f8907e20d46708cb2487e011ba5c4

--- assets/entries/pages_docs_basic.BWKGSDLj.js
+++ assets/entries/pages_docs_basic.BWKGSDLj.js
@@ -1,4 +1,4 @@
-import{j as t,C as e,i as r,o as l,a as p}from"../chunks/chunk-uRu001Rx.js";import{S as n,a as i,b as s,c as a,i as u}from"../chunks/chunk-cgnbRicC.js";import{D as c,H as o}from"../chunks/chunk-DkjftF2P.js";import{D as d}from"../chunks/chunk-DwxspIL1.js";/* empty css                      */import"../chunks/chunk-CYDrz2XN.js";/* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-D7HrI6pR.js";const m=`export const CustomButton = rc.button\`
+import{j as t,C as e,i as r,o as l,a as p}from"../chunks/chunk-B9McCG0N.js";import{S as n,a as i,b as s,c as a,i as u}from"../chunks/chunk-ff6yEUbw.js";import{D as c,H as o}from"../chunks/chunk-DDFPY9kE.js";import{D as d}from"../chunks/chunk-DJ-5Kb2b.js";/* empty css                      */import"../chunks/chunk-CYDrz2XN.js";/* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-D7HrI6pR.js";const m=`export const CustomButton = rc.button\`
   text-blue
   px-3
   py-2
